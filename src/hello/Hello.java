e50766708f5e21ea4dee46c1daeac52a9982ddc9
luwei-pixsel <735919786@163.com>
2022-07-18 23:54:37 +0800

    v0.2

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -1,10 +1,14 @@
 package hello;
 
+import java.util.Scanner;
+
 public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world!");
+		Scanner in=new Scanner(System.in);
+		System.out.println(in.nextLine());
 
 	}
 
