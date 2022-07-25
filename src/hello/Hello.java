1854ee8463afcee31e039db686ce389b9b1c2ca5
luwei-pixsel <735919786@163.com>
2022-07-25 20:47:00 +0800

    v0.3整数浮点数强制类型转换

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -6,9 +6,16 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hello world!");
+//		System.out.println("hello world!");
 		Scanner in=new Scanner(System.in);
-		System.out.println(in.nextLine());
+		int inch;
+		int inc;
+		double hight;
+//		System.out.println(in.nextLine());
+//		int value=in.nextInt();
+		inch=in.nextInt();
+		inc=in.nextInt();
+		System.out.println((int)((inch+inc/12.0)*0.3048*100)+"cm");
 
 	}
 
